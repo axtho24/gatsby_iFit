b6521d8fa4aeb3956a4bc6e3b1bff4fdedd5f46d
Alexander Thomas <aletho24@gmail.com>
2020-08-14 06:38:11 -0500

    Update headerTopper.js

--- src/components/headerTopper.js
+++ src/components/headerTopper.js
@@ -5,7 +5,7 @@ const HeadTop = () => (
   <Container fluid>
   <Row className="navTopper border-bottom">
     <Col className="border-right text-center" xl={1} lg={2} md={2} sm={4} xs={4}>
-      <p><a href="/">BLOG</a></p>
+      <p><a href="/">BLOGS</a></p>
     </Col>
     <Col className="border-right text-center" xl={1} lg={2} md={2} sm={4} xs={4}>
       <p><a href="/">NOURISH</a></p>
